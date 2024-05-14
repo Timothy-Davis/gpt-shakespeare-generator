e06429b42d57128c96363253c56b3ef1e2dd99c0
timothy-davis <dtimmy690@gmail.com>
2024-05-14 17:01:41 -0500

    Put data into a torch tensor and split the data into training and testing sets.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+import torch
+
 VOCAB = ""
 VOCAB_SIZE = 0
+TRAINING_SIZE = 0.9
+BLOCK_SIZE = 8
+BATCH_SIZE = 4
+torch.manual_seed(1337)
 
 
 def string_to_integer():
@@ -24,3 +30,22 @@ if __name__ == "__main__":
 
     VOCAB = sorted(list(set(text)))
     VOCAB_SIZE = len(VOCAB)
+
+    data = torch.tensor(encode(text), dtype=torch.long)
+    train_data = data[:int(len(data)*TRAINING_SIZE)]
+    test_data = data[int(len(data)*TRAINING_SIZE):]
+
+    def get_batch(split):
+        # generate a small batch of data of inputs x and targets y
+        data = train_data if split == 'train' else test_data
+        ix = torch.randint(len(data) - BLOCK_SIZE, (BATCH_SIZE,))
+        x = torch.stack([data[i:i+BLOCK_SIZE] for i in ix])
+        y = torch.stack([data[i+1:i+BLOCK_SIZE+1] for i in ix])
+        return x, y
+
+    xb, yb = get_batch('train')
+    for b in range(BATCH_SIZE):
+        for t in range(BLOCK_SIZE):
+            context = xb[b, :t+1]
+            target = yb[b, t]
+            print(f"When input is {context.tolist()} the target is: {target}")
