685d006dcc3f58ffa5cdeb9d2301e65410e5ee77
timothy-davis <dtimmy690@gmail.com>
2024-05-19 16:02:50 -0500

    Reformated to more idiosyncratic Python scripting format. Added Bigram Language Model.

--- src/main.py
+++ src/main.py
@@ -1,51 +1,104 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
-VOCAB = ""
-VOCAB_SIZE = 0
-TRAINING_SIZE = 0.9
-BLOCK_SIZE = 8
-BATCH_SIZE = 4
+# Hyperparameters
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+# ----------------
 torch.manual_seed(1337)
 
+with open('../tiny-shakespeare.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
 
-def string_to_integer():
-    return {ch: i for i, ch in enumerate(VOCAB)}
+# Unique characters / Vocabulary
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
 
+# Functions that map characters to integers
+string_to_int = {ch: i for i, ch in enumerate(chars)}
+int_to_string = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [string_to_int[c] for c in s]
+decode = lambda l: ''.join([int_to_string[i] for i in l])
 
-def integer_to_string():
-    return {i: ch for i, ch in enumerate(VOCAB)}
+# Setup for training and testing splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
 
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
 
-def encode(chars):
-    return [string_to_integer()[c] for c in chars]
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
 
+    return out
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = torch.nn.Embedding(vocab_size, vocab_size)
 
-def decode(chars):
-    return ''.join([integer_to_string()[i] for i in chars])
+    def forward(self, idx, targets=None):
+        logits = self.token_embedding_table(idx)
 
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = torch.nn.functional.cross_entropy(logits, targets)
 
-if __name__ == "__main__":
-    with open('../tiny-shakespeare.txt', 'r', encoding='utf-8') as file:
-        text = file.read()
+        return logits, loss
 
-    VOCAB = sorted(list(set(text)))
-    VOCAB_SIZE = len(VOCAB)
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:,-1,:]
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
 
-    data = torch.tensor(encode(text), dtype=torch.long)
-    train_data = data[:int(len(data)*TRAINING_SIZE)]
-    test_data = data[int(len(data)*TRAINING_SIZE):]
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
 
-    def get_batch(split):
-        # generate a small batch of data of inputs x and targets y
-        data = train_data if split == 'train' else test_data
-        ix = torch.randint(len(data) - BLOCK_SIZE, (BATCH_SIZE,))
-        x = torch.stack([data[i:i+BLOCK_SIZE] for i in ix])
-        y = torch.stack([data[i+1:i+BLOCK_SIZE+1] for i in ix])
-        return x, y
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
     xb, yb = get_batch('train')
-    for b in range(BATCH_SIZE):
-        for t in range(BLOCK_SIZE):
-            context = xb[b, :t+1]
-            target = yb[b, t]
-            print(f"When input is {context.tolist()} the target is: {target}")
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
